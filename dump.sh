fc2f88ff54c241251a06e3c6582ebfb9bab7dc1d
zhoudd1 <15019442511@163.com>
2018-09-18 15:10:16 +0800

    Update dump.sh

--- dump.sh
+++ dump.sh
@@ -9,9 +9,9 @@ i=`expr $i + 1`;
 
 time=$(date "+%Y-%m-%d %H:%M:%S")
 
-free | sed -n '2p;2q' | sed -e "s/Mem:/$time/g" >> ./date/mem.log
+free | sed -n '2p;2q' | sed -e "s/Mem:/$time/g" >> mem.log
 
-free | sed -n '2p;2q' | sed -e "s/Mem:/$i/g" >> ./date/mem1.log
+free | sed -n '2p;2q' | sed -e "s/Mem:/$i/g" >> mem1.log
 
 sleep 1
 
